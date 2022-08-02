684c2170745e9393b068dea9c7703f59f5315af7
Bryan Askin <bryana@bamboomcloud.com>
2022-08-02 11:48:21 -0700

    Created models for audio files and metadata.

---
+++ Logcast.Recruitment.Shared/Models/AudioFileModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Logcast.Recruitment.Shared.Models
+{
+    public class AudioFileModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public string Extension { get; set; }
+        public byte[] Data { get; set; }
+        public int MetaDataId { get; set; }
+    }
+}
---
+++ Logcast.Recruitment.Shared/Models/MetaDataModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Logcast.Recruitment.Shared.Models
+{
+    public class MetaDataModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string Album { get; set; }
+        public string Genre { get; set; }
+        public int Bitrate { get; set; }
+        public long Duration { get; set; }
+    }
+}
